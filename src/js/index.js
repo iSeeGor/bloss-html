3ca164ce9875bf586338fc4544e4efbb84f55d4b
iSeeGor <iseegor@gmail.com>
2021-04-26 10:17:07 +0300

    add responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', function(){
-
+	
+	hamburger();
+	categoryToggle();
 	testimonialSlider();
 	// All Pages
 	// currencyDropdown();
@@ -24,6 +26,34 @@ window.addEventListener('DOMContentLoaded', function(){
 	// testimonialHomeReadMore();
 	
 });
+const hamburger = () => {
+
+	document.querySelector('.hamburger').addEventListener('click', function(e) {
+		this.classList.toggle('is-active');
+
+		document.querySelector('.header').classList.toggle('is-active');
+	});
+}
+
+const categoryToggle = () => {
+
+	document.querySelector('.hero-control__categories').addEventListener('click', function(){
+
+		document.querySelector('.hero-categories').classList.add('is-active');
+		document.body.classList.add('glass-overflow-in');
+	});
+
+	document.querySelector('.hero-categories ._close').addEventListener('click', function(){
+
+		document.querySelector('.hero-categories').classList.remove('is-active');
+		document.body.classList.remove('glass-overflow-in');
+		document.body.classList.add('glass-overflow-out');
+
+		setTimeout(() => {
+			document.body.classList.remove('glass-overflow-out');
+		}, 1000);
+	});
+}
 
 const testimonialSlider = () => {
 
@@ -41,7 +71,7 @@ const testimonialSlider = () => {
 
 		pagination: {
 
-			el: '.testimonial-slider__paginaion',
+			el: '.testimonial-slider__pagination',
 			type: 'bullets',
 		}
 	});
